d30790232a182a098025bc7fcd1dc0c293a8f897
Semyon Kushnarenko <trisemya@gmail.com>
2023-02-10 00:32:46 +0300

    feat: add all required abilities

---
+++ src/components/UserInfo/UserInfo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="user">
+    <div class="user__logo"></div>
+    <div class="user__dropdown">
+      <img class="user__icon" src="/assets/user.png" alt="">
+      <span class="user__name">Alexander Borisenko</span>
+      <span class="arrow"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserInfo"
+}
+</script>
+
+<style scoped>
+.user__dropdown {
+  cursor: pointer;
+  display: flex;
+  gap: .5rem;
+  align-items: center;
+}
+.user__icon {
+  height: 15px;
+}
+.arrow {
+  width: 0;
+  aspect-ratio: 0;
+  border: 5px solid transparent;
+  border-top-color: #000;
+}
+</style>
---
+++ src/components/partials/HeaderComponent/HeaderComponent.vue
@@ -0,0 +1,64 @@
+<template>
+<header class="header">
+  <div class="container container_header">
+    <h1>Movie Catalog</h1>
+    <div class="header__main">
+      <SearchComponent
+          :searchQuery="searchQuery"
+          @text-change="onSearchChange"
+      />
+      <UserInfo />
+    </div>
+  </div>
+</header>
+</template>
+
+<script>
+import UserInfo from "@/components/UserInfo/UserInfo.vue";
+import SearchComponent from "@/components/SearchComponent/SearchComponent.vue";
+
+export default {
+  name: "HeaderComponent",
+  components: {SearchComponent, UserInfo},
+  props: {
+    onSearchChange: {
+      type: Function,
+      required: true,
+    },
+    searchQuery: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style>
+.container_header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.header {
+  background: #42b983;
+  width: 100%;
+  padding: 1rem 2rem;
+}
+.header__main {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+@media screen and (max-width: 850px) {
+  .container_header {
+    flex-direction: column;
+    gap: 1rem;
+  }
+}
+@media screen and (max-width: 640px) {
+  .header__main {
+    flex-direction: column;
+    gap: 1rem;
+  }
+}
+</style>
